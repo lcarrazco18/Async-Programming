fc2fd1bd0a1ce3cb52864565755c2ed5afebb7e7
Luis Carrazco <luiscarrazco@gmail.com>
2019-06-18 19:30:55 -0600

    Getting Ajax started

---
+++ apps.js
@@ -0,0 +1,21 @@
+document.getElementById('button').addEventListener('click', loadData);
+
+function loadData() {
+    // Create an XHR Object
+    const xhr = new XMLHttpRequest();
+
+    // Open
+    xhr.open('GET', 'data.txt', true);
+
+    xhr.onload = function() {
+        if(this.status === 200) {
+            console.log(this.responseText);
+        }
+    }
+}
+
+
+// HTTP Statuses
+    // 200: "OK"
+    // 403: "Forbidden"
+    // 404: "Not Found"

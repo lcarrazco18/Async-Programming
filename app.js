ec9248264fbe3bc070c66893f7b9a6930b52d6b3
Luis Carrazco <luiscarrazco@gmail.com>
2019-07-05 18:47:32 -0600

    got button1 getting customer info.

---
+++ app.js
@@ -0,0 +1,15 @@
+document.getElementById('button1').addEventListener('click', loadCustomer);
+
+function loadCustomer() {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'customer.json', true);
+
+    xhr.onload = function(){
+        if(this.status === 200) {
+            console.log(this.responseText);
+        }
+    }
+
+    xhr.send();
+}
